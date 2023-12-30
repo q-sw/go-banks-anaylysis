61c56a1c58d23495b55b99483060f1e75b3f4216
q-sw <quentin@q-sw.xyz>
2023-12-30 15:01:59 +0100

    [ADD] Create Db connect and schema

---
+++ storage/storage.go
@@ -0,0 +1,95 @@
+package storage
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"os"
+)
+
+type Storage interface {
+	CreateTransaction() error
+	UpdateTransaction() error
+	GetTransaction() error
+	GetAllTransaction() error
+}
+
+type PostgreSQL struct {
+	db *sql.DB
+}
+
+func NewConnection() (*PostgreSQL, error) {
+	db_user := os.Getenv("DB_USER")
+	db_pwd := os.Getenv("DB_PWD")
+	db_name := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("user=%v dbname=%v password=%v host='172.17.0.2' port='5432' sslmode=disable", db_user, db_name, db_pwd)
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connect")
+	return &PostgreSQL{db: db}, nil
+}
+
+func (s *PostgreSQL) Init() error {
+	c := s.createCategoriesTable()
+	if c != nil {
+		return c
+	}
+
+	t := s.createTransactionTable()
+	if t != nil {
+		return t
+	}
+
+	b := s.createBalanceTable()
+	if b != nil {
+		return b
+	}
+
+	return nil
+}
+
+func (s *PostgreSQL) createTransactionTable() error {
+	query := `create table if not exists transactions (
+        id int not null primary key,
+        transction_id varchar(250),
+        amount varchar(250),
+        code varchar(250),
+        description varchar(250),
+        categoryID int references categories(id)
+    )`
+
+	_, err := s.db.Exec(query)
+	return err
+
+}
+
+func (s *PostgreSQL) createCategoriesTable() error {
+	query := `create table if not exists categories (
+        id int not null primary key,
+        name varchar(250)
+    )`
+
+	_, err := s.db.Exec(query)
+	return err
+
+}
+func (s *PostgreSQL) createBalanceTable() error {
+	query := `create table if not exists balance (
+        id serial primary key,
+        date varchar(250),
+        amount varchar(250)
+    )`
+
+	_, err := s.db.Exec(query)
+	return err
+
+}
